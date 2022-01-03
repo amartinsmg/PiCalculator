840215898bda0ac0048d479e26c978e0fca15f26
amartinsmg <andremindurimg@gmail.com>
2022-01-02 21:54:51 -0300

    InitialCommit

---
+++ Pi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    double pi = 0;
+    float k = 1;
+    for (int i = 0; i < 1000000; i++) {
+        if (i % 2 == 0) {
+            pi += 4.0/k;
+        }else {
+            pi -= 4.0/k;
+        }
+        k += 2;
+    }
+    printf("Pi = %f \n", pi);
+    return 0;
+}
---
+++ Pi.py
@@ -0,0 +1,13 @@
+pi = 0
+
+k = 1
+
+for i in range(10000000):
+    if (i % 2 == 0):
+        pi += 4/k
+    else:
+        pi -= 4/k
+
+    k += 2
+
+print("Pi = " + str(pi))
