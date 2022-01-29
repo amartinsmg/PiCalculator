45334b5515895b928add28016954f25613211e67
amartinsmg <andremindurimg@gmail.com>
2022-01-29 12:34:12 -0300

    Formatting

--- Pi.c
+++ Pi.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    double pi = 0;
-    float k = 1;
-    for (int i = 0; i < 1000000; i++) {
-        if (i % 2 == 0) {
-            pi += 4.0/k;
-        }else {
-            pi -= 4.0/k;
-        }
-        k += 2;
+int main()
+{
+  double pi = 0;
+  float k = 1;
+  for (int i = 0; i < 1000000; i++)
+  {
+    if (i % 2 == 0)
+    {
+      pi += 4.0 / k;
     }
-    printf("Pi = %f \n", pi);
-    return 0;
+    else
+    {
+      pi -= 4.0 / k;
+    }
+    k += 2;
+  }
+  printf("Pi = %f \n", pi);
+  return 0;
 }
--- Pi.py
+++ Pi.py
@@ -2,11 +2,11 @@ pi = 0
 k = 1
 
 for i in range(10000000):
-    if (i % 2 == 0):
-        pi += 4/k
-    else:
-        pi -= 4/k
+  if (i % 2 == 0):
+    pi += 4/k
+  else:
+    pi -= 4/k
 
-    k += 2
+  k += 2
 
 print("Pi = " + format(pi, ".7f"))
