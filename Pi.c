dacd384ce022985bd0ffee71fe7781d1e13a6e1a
amartinsmg <andremindurimg@gmail.com>
2022-04-27 08:29:05 -0300

    C update

--- Pi.c
+++ Pi.c
@@ -3,19 +3,19 @@
 int main()
 {
   double pi = 0,
-   k = 1;
+         k = 1;
   for (int i = 0; i < 1000000000; i++)
   {
     if (i % 2 == 0)
     {
-      pi += 4.0 / k;
+      pi += 4 / k;
     }
     else
     {
-      pi -= 4.0 / k;
+      pi -= 4 / k;
     }
     k += 2;
   }
-  printf("Pi = %.9f \n", pi);
+  printf("Pi = %.9f\n", pi);
   return 0;
 }
