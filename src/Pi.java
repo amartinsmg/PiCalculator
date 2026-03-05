757d832b7e08cc6cce2e1c8dda00ce4498809292
amartinsmg <andremindurimg@gmail.com>
2026-03-05 00:59:37 -0300

    Rename s variable in Pi.java

--- src/Pi.java
+++ src/Pi.java
@@ -1,16 +1,16 @@
 public class Pi {
   public static double calculatePi(int precision) {
-    double pi = 0,
+    double s = 0,
         k = 1;
     int max = (int) Math.pow(10, precision);
     for (int i = 0; i < max; i++) {
       if (i % 2 == 0)
-        pi += 4 / k;
+        s += 4 / k;
       else
-        pi -= 4 / k;
+        s -= 4 / k;
       k += 2;
     }
-    return pi;
+    return s;
   }
 
   public static void main(String[] args) {
