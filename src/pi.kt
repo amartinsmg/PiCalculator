a750b5495a5fb25299d12c8ecf88eeb0bda8ec1e
amartinsmg <andremindurimg@gmail.com>
2026-03-05 11:32:11 -0300

    Update pi.kt to improve readability

--- src/pi.kt
+++ src/pi.kt
@@ -2,11 +2,10 @@ fun calculatePi(precision: Int): Double {
   var (s: Double, k: Double) = Pair(0.0, 1.0)
   var max: Int = Math.pow(10.0, precision.toDouble()).toInt()
   for (i in 0 until max) {
-    if (i % 2 == 0) {
+    if (i % 2 == 0) 
       s += 4 / k
-    } else {
+    else 
       s -= 4 / k
-    }
     k += 2
   }
   return s
