e18d80514158ea6cd79b846bb1f634005f891780
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:17:49 -0300

    Rename s variable in pi.go

--- src/pi.go
+++ src/pi.go
@@ -6,18 +6,18 @@ import (
 )
 
 func calculatePi(precision int32) float64 {
-  var pi, k float64 = 0, 1
+  var s, k float64 = 0, 1
   var max, i int32
   max = int32(math.Pow(10, float64(precision)))
   for i = 0; i < max; i++ {
     if i%2 == 0 {
-      pi += 4 / k
+      s += 4 / k
     } else {
-      pi -= 4 / k
+      s -= 4 / k
     }
     k += 2
   }
-  return pi
+  return s
 }
 
 func main() {
