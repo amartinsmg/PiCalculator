0d714006c43dcde64dfd655a995e634e4e51a552
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:05:05 -0300

    Rename s variable in pi.js

--- src/pi.js
+++ src/pi.js
@@ -1,13 +1,13 @@
 function calculatePi(precision) {
-  let pi = 0,
+  let s = 0,
     k = 1,
     max = 10 ** precision;
   for (let i = 0; i < max; i++) {
-    if (i % 2 === 0) pi += 4 / k;
-    else pi -= 4 / k;
+    if (i % 2 === 0) s += 4 / k;
+    else s -= 4 / k;
     k += 2;
   }
-  return pi;
+  return s;
 }
 
 let pi = calculatePi(9);
