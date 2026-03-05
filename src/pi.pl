4b3a5e154c2f55586c22cdd196f355ee383489e3
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:16:41 -0300

    Rename $s variable in pi.pl

--- src/pi.pl
+++ src/pi.pl
@@ -3,18 +3,18 @@
 sub calculate_pi
 {
   $precision = $_[0];
-  $pi = 0;
+  $s = 0;
   $k = 1;
   $max = 10 ** $precision;
   for ($i = 0; $i < $max; $i++) {
     if ($i % 2 == 0) {
-      $pi += 4 / $k;
+      $s += 4 / $k;
     } else {
-      $pi -= 4 / $k;
+      $s -= 4 / $k;
     }
     $k += 2;
   }
-  return $pi;
+  return $s;
 }
 
 $pi = calculate_pi(9);
