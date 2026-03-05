4187d2fed5d8b6ac21975ee26ce5547ef95c4d4c
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:10:02 -0300

    Rename s variable in pi.rb and fix range

--- src/pi.rb
+++ src/pi.rb
@@ -1,16 +1,16 @@
 def calculate_pi(precision)
-  pi = 0.0
+  s = 0.0
   k = 1.0
   max = 10 ** precision
-  for i in 0..max do
+  for i in 0...max do
     if i % 2 == 0
-      pi += 4 / k
+      s += 4 / k
     else
-      pi -= 4 / k
+      s -= 4 / k
     end
     k += 2
   end
-  return pi
+  return s
 end
 
 pi = calculate_pi(9)
