39b4ef4fa7508dcfa3659cf98e3326f909636de0
amartinsmg <andremindurimg@gmail.com>
2026-03-05 00:52:55 -0300

    Rename s variable in pi.dart

--- src/pi.dart
+++ src/pi.dart
@@ -1,17 +1,17 @@
 import 'dart:math';
 
 double calculatePi(precision) {
-  double pi = 0, k = 1;
+  double s = 0, k = 1;
   int max = pow(10, precision).toInt();
   for (var i = 0; i < max; i++) {
     if (i % 2 == 0) {
-      pi += 4 / k;
+      s += 4 / k;
     } else {
-      pi -= 4 / k;
+      s -= 4 / k;
     }
     k += 2;
   }
-  return pi;
+  return s;
 }
 
 main() {
