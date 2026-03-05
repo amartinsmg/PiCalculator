169e99cb01318f6657a542debb1cb8b6a3096de1
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:14:37 -0300

    Rename s variable in pi.rs

--- src/pi.rs
+++ src/pi.rs
@@ -1,15 +1,15 @@
 fn calculate_pi(precision: i32) -> f64 {
-  let (mut pi, mut k): (f64, f64) = (0.0, 1.0);
+  let (mut s, mut k): (f64, f64) = (0.0, 1.0);
   let max: i32 = i32::pow(10, precision as u32);
   for i in 0..max {
     if i % 2 == 0 {
-      pi = pi + 4.0 / k;
+      s = s + 4.0 / k;
     } else {
-      pi = pi - 4.0 / k;
+      s = s - 4.0 / k;
     }
     k = k + 2.0;
   }
-  return pi;
+  return s;
 }
 
 fn main() {
