588feb57fce12b8c40568f2f738299b318ed5a29
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:15:54 -0300

    Rename s variable in pi.cs

--- src/pi.cs
+++ src/pi.cs
@@ -6,18 +6,18 @@ namespace Pi
   {
     static double CalculatePi(int precision)
     {
-      double pi = 0,
+      double s = 0,
               k = 1;
       int max = (int) Math.Pow(10, precision);
       for (int i = 0; i < max; i++)
       {
         if (i % 2 == 0)
-          pi += 4 / k;
+          s += 4 / k;
         else
-          pi -= 4 / k;
+          s -= 4 / k;
         k += 2;
       }
-      return pi;
+      return s;
     }
     static void Main(string[] args)
     {
