51a246bc0f871b65eb79ff901971efd741606e89
amartinsmg <andremindurimg@gmail.com>
2026-03-05 00:58:26 -0300

    Rename s and pi_val variables in pi.f90

--- src/pi.f90
+++ src/pi.f90
@@ -1,23 +1,23 @@
 program Pi
-  real(8) :: pi_
-  pi_ = calculate_pi(9)
-  print "(A, F10.8)", "Pi = ", pi_
+  real(8) :: pi_val
+  pi_val = calculate_pi(9)
+  print "(A, F10.8)", "Pi = ", pi_val
 
 contains
   function calculate_pi(precision)
-    real(8) :: calculate_pi, pi_, k
+    real(8) :: calculate_pi, s, k
     integer :: precision, i, max
-    pi_ = 0
+    s = 0
     k = 1
     max = 10 ** precision
-    do i = 0, max
+    do i = 0, (max - 1)
       if (mod(i, 2) == 0) then
-        pi_ = pi_ + 4 / k
+        s = s + 4 / k
       else
-        pi_ = pi_ - 4 / k
+        s = s - 4 / k
       end if
       k = k + 2
     end do
-    calculate_pi = pi_
+    calculate_pi = s
   end function calculate_pi
 end program Pi
