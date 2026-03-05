bc6a3c5e6bff51d3b97ce1089693e7d8229fe02f
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:18:50 -0300

    Rename s variable in pi.kt and fix range

--- src/pi.kt
+++ src/pi.kt
@@ -1,7 +1,7 @@
 fun calculatePi(precision: Int): Double {
   var (pi: Double, k: Double) = Pair(0.0, 1.0)
   var max: Int = Math.pow(10.0, precision.toDouble()).toInt()
-  for (i in 0..max) {
+  for (i in 0 until max) {
     if (i % 2 == 0) {
       pi += 4 / k
     } else {
