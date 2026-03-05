2fb128ffa14e13e71597812f39ffeb185ae7ea80
amartinsmg <andremindurimg@gmail.com>
2026-03-05 11:27:42 -0300

    Update pi.js to improve readability

--- src/pi.js
+++ src/pi.js
@@ -3,8 +3,10 @@ function calculatePi(precision) {
     k = 1,
     max = 10 ** precision;
   for (let i = 0; i < max; i++) {
-    if (i % 2 === 0) s += 4 / k;
-    else s -= 4 / k;
+    if (i % 2 === 0)
+      s += 4 / k;
+    else
+      s -= 4 / k;
     k += 2;
   }
   return s;
