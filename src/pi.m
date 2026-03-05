adc6b071d228509dc60008472981473bc11041ab
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:06:35 -0300

    Rename s and val_pi variables in pi.m

--- src/pi.m
+++ src/pi.m
@@ -1,16 +1,16 @@
-function _pi = calculatepi(precision)
-  _pi = 0;
+function s = calculatepi(precision)
+  s = 0;
   k = 1;
   max = 10 ^ precision;
-  for i_ = 0: max
+  for i_ = 0:(max - 1)
     if(mod(i_, 2) == 0)
-      _pi += 4 / k;
+      s += 4 / k;
     else
-      _pi -= 4 / k;
+      s -= 4 / k;
     end
     k += 2;
   end
 end
 
-_pi = calculatepi(9);
-printf("Pi = %.8f\n", _pi)
+val_pi = calculatepi(9);
+printf("Pi = %.8f\n", val_pi)
