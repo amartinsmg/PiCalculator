36a0574339f28acdfde0bc0954ac971c450edc9a
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:02:21 -0300

    Rename s and ValPi variables in pi.pp

--- src/pi.pp
+++ src/pi.pp
@@ -1,24 +1,24 @@
 PROGRAM Pi;
 USES Math;
-VAR _Pi: Real;
+VAR ValPi: Real;
 
 FUNCTION CalculatePi(Precision: LongInt): Real;
-VAR _Pi, k: Real;
+VAR s, k: Real;
   i, Max: LongInt;
 BEGIN
-  _Pi := 0;
+  s := 0;
   k := 1;
   Max := Trunc(Power(10, Precision));
   FOR i := 0 TO Max DO
   BEGIN
-    IF (i MOD 2 = 0) THEN _Pi := _Pi + 4 / k
-    ELSE _Pi := _Pi - 4 / k;
+    IF (i MOD 2 = 0) THEN s := s + 4 / k
+    ELSE s := s - 4 / k;
     k := k + 2;
   END;
-  CalculatePi := _Pi;
+  CalculatePi := s;
 END;
 
 BEGIN
-  _Pi := CalculatePi(9);
-  Writeln('Pi = ', _Pi:1:8);
+  ValPi := CalculatePi(9);
+  Writeln('Pi = ', ValPi:1:8);
 END.
