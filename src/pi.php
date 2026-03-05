6875cc5ccac3510d4471e18d3c2b2124c8e36b6f
amartinsmg <andremindurimg@gmail.com>
2026-03-05 01:21:49 -0300

    Rename $s variable in pi.php

--- src/pi.php
+++ src/pi.php
@@ -1,17 +1,17 @@
 <?php
 function calculatePi($precision)
 {
-  $pi = 0.0;
+  $s = 0.0;
   $k = 1.0;
   $max = pow(10, $precision);
   for ($i = 0; $i < $max; $i++) {
     if ($i % 2 == 0)
-      $pi += 4 / $k;
+      $s += 4 / $k;
     else
-      $pi -= 4 / $k;
+      $s -= 4 / $k;
     $k += 2;
   }
-  return $pi;
+  return $s;
 }
 
 $pi = calculatePi(9);
