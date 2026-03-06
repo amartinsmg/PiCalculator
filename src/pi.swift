a3ae32699526c22579824f0df468017b98b23d9c
amartinsmg <andremindurimg@gmail.com>
2026-03-05 23:38:57 -0300

    Add pi.swift

---
+++ src/pi.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func calculatePi(precision: Int) -> Double {
+  var s = 0.0
+  var k = 1.0
+  let max = Int(pow(10.0, Double(precision)))
+  for i in 0..<max {
+    if i % 2 == 0 {
+      s += 4 / k
+    } else {
+      s -= 4 / k
+    }
+    k += 2
+  }
+  return s
+}
+
+var pi = calculatePi(precision: 9)
+print(String(format: "Pi = %.8f", pi))
